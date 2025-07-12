aeb1161e887d489c5de0a67f341d016327e70217
Kostiantyn Matvieienkov <kostya.code@gmail.com>
2025-07-12 20:31:26 +0100

    Initial commit.

---
+++ src/zero-proof-pw-manager-frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { zero_proof_pw_manager_backend } from 'declarations/zero-proof-pw-manager-backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    zero_proof_pw_manager_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
