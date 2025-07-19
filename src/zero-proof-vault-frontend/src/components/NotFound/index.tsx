3471847a3aef18cda7b15d29a477c2c68aa77c7d
Kostiantyn Matvieienkov <kostya.code@gmail.com>
2025-07-19 20:40:04 +0100

    add self-managed identity system

---
+++ src/zero-proof-vault-frontend/src/components/NotFound/index.tsx
@@ -0,0 +1,23 @@
+// import React from "react";
+// import { Result, Button } from "antd";
+// import { Link } from "react-router-dom";
+// import { useIdentitySystem } from "../../framework/identity";
+
+// const NotFoundPage = () => {
+//   const { wrapOrgCode } = useIdentitySystem();
+//   return (
+//     <Result
+//       status="404"
+//       title="404 Not Found"
+//       subTitle="Sorry, the page you visited does not exist."
+//       extra={
+//         <Link to={wrapOrgCode("/drive")}>
+//           <Button type="primary">Go Home</Button>
+//         </Link>
+//       }
+//       style={{ marginTop: "10vh" }}
+//     />
+//   );
+// };
+
+// export default NotFoundPage;
